8bfa5a2e28058b6c6b38c14fa54a93c40d0b2317
Tom Davies <tom.tdigital@gmail.com>
2018-08-02 15:43:37 +0100

    styled with own branding

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,6 +4,7 @@ const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
+const home = document.querySelector('#home');
 const navItems = document.querySelectorAll('.nav-item');
 
 //Set initial state of menu
@@ -17,7 +18,8 @@ function toggleMenu() {
         menuBtn.classList.add('close');
         menu.classList.add('show');
         menuNav.classList.add('show');
-        menuBranding.classList.add('show')
+        menuBranding.classList.add('show');
+        home.classList.add('more-opacity');
         navItems.forEach(item => item.classList.add('show'));
         //Set menu state now it's open
         showMenu = true;
@@ -28,6 +30,7 @@ function toggleMenu() {
         menu.classList.remove('show');
         menuNav.classList.remove('show');
         menuBranding.classList.remove('show')
+        home.classList.remove('more-opacity');
         navItems.forEach(item => item.classList.remove('show'));
         //You must re-set the menu state or it will stay as 'true' and won't toggle
         showMenu = false;
@@ -35,3 +38,10 @@ function toggleMenu() {
     }
 }
 
+var portrait = document.getElementById("changepic");
+    portrait.onmouseenter = function(){
+       this.src = "./img/tDigitalLogo2.jpg";
+    }
+    portrait.onmouseleave = function(){
+        this.src = "./img/profilepic2.jpg";
+     }
